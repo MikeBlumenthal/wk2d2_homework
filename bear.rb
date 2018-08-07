a0fd8715e70149290879ce003642a4ae681f1fa8
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-07 17:43:34 +0100

    finished extensions

--- bear.rb
+++ bear.rb
@@ -1,11 +1,29 @@
 class Bear
 
-  attr_reader :name, :type, :stomach
+  attr_reader :name, :type, :voice, :stomach
+  attr_accessor :stomach
 
-  def initialize(name, type, stomach = [])
+  def initialize(name, type, voice, stomach = [])
     @name = name
     @type = type
+    @voice = voice
     @stomach = stomach
+  end
+
+  def eats_fish(river)
+    @stomach << river.lose_fish
+  end
+
+  def roar
+    return "#{@voice}!"
+  end
+
+  def stomach_contents
+    if @stomach.length > 0
+      return "I've had #{@stomach.length} fish!"
+    else
+      return "I'm hungry!"
+    end
   end
 
 end
--- river.rb
+++ river.rb
@@ -1,10 +1,19 @@
 class River
 
   attr_reader :name, :fish
+  attr_accessor :fish
 
   def initialize(name, fish = [])
     @name = name
     @fish = fish
+  end
+
+  def count_fish
+    return @fish.length
+  end
+
+  def lose_fish
+    return @fish.pop
   end
 
 end
